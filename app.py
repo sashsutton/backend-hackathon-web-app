a1c1264e420f52e98560f51df9ec87ddc8136265
zaiba <fadila.zaiba@etu.univ-amu.fr>
2026-02-25 10:33:48 +0100

    clerk test config

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
+import os
+from clerk_backend_api import Clerk
+from dotenv import load_dotenv
 from flask import Flask
 
-app = Flask(__name__)
+load_dotenv("key.env") # <--- Correction ici
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/test')
+def test_clerk():
+    with Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY")) as sdk:
+        try:
+            response = sdk.users.list()
+            
+            nb_users = len(response)
+            
+            return f"Connexion OK  {nb_users}"
+        
+        except Exception as e:
+            return f"Erreur de connexion : {str(e)}"
 
+@app.route('/create-test')
+def create_user():
+    with Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY")) as sdk:
+        try:
+            new_user = sdk.users.create(
+                email_address=["test-user@example.com"], 
+                password="MotDePasseTresSecurise123!",
+                first_name="fadila",
+                last_name="zaiba"
+            )
+            
+            return f"<h1>Succès !</h1>Utilisateur créé avec l'ID : <b>{new_user.id}</b>"
+        
+        except Exception as e:
+            return f"<h1>Erreur</h1> {str(e)}"
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
